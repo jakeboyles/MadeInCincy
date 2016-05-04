14c72c3c8dfc297774ccf3a00cd147d382d5b8e2
Jake Boyles <jake@jakeboyles.com>
2016-05-03 22:39:01 -0500

    Update main.js

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -7,8 +7,8 @@ var map = L.map('map').setView([39.1300, -84.5167], 12);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
-    id: 'jakeboyles.j0ajipap',
-    accessToken: 'pk.eyJ1IjoiamFrZWJveWxlcyIsImEiOiJNcGJpWXhJIn0.ONDjoScLnbU4_VVfXmeIAA'
+    id: '',
+    accessToken: ''
 }).addTo(map);
 
 
